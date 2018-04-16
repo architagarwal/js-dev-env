cb24fe30064db83531585b9633fa657116128842
Archit Agarwal <archit.agarwal26@gmail.com>
2018-04-16 00:45:57 -0700

    module 9 work in progress

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,2 @@
 import chalk from 'chalk';
-//var chalk = require('chalk');
-
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
